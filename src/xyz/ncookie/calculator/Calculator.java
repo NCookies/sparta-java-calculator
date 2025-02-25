478d53ff10bb8e91a6e97b9e1e5802f92799882d
SeungWoo Ryu <swstar21c@gmail.com>
2025-02-25 16:34:37 +0900

    #2 - 양의 정수(0 포함) 입력
    - 0과 양의 정수가 아닌 숫자가 입력되면 메세지 출력

--- src/xyz/ncookie/calculator/Calculator.java
+++ src/xyz/ncookie/calculator/Calculator.java
@@ -1,7 +1,21 @@
 package xyz.ncookie.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 양의 정수 입력: ");
+        int a = sc.nextInt();
+
+        System.out.print("두 번째 양의 정수 입력: ");
+        int b = sc.nextInt();
+
+        if (a < 0 || b < 0) {
+            System.out.println("0을 포함한 양의 정수만 입력할 수 있습니다.");
+        }
     }
+
 }
