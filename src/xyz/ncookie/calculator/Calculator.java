645742326232e39abe8da5485e36b15f4abe0227
SeungWoo Ryu <swstar21c@gmail.com>
2025-02-25 17:01:16 +0900

    #2 - 계산 완료 후 특정 문자열 입력받기 전까지 무한루프 돌도록 구현
    - "exit" 키워드를 입력받기 전까지 계산기 기능 루프를 무한으로 반복한다.

--- src/xyz/ncookie/calculator/Calculator.java
+++ src/xyz/ncookie/calculator/Calculator.java
@@ -7,58 +7,64 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 양의 정수 입력: ");
-        int a = sc.nextInt();
+        while (true) {
+            System.out.print("첫 번째 양의 정수 입력: ");
+            int a = sc.nextInt();
 
-        System.out.print("두 번째 양의 정수 입력: ");
-        int b = sc.nextInt();
+            System.out.print("두 번째 양의 정수 입력: ");
+            int b = sc.nextInt();
 
-        System.out.print("사칙연산 기호 입력: ");
-        char operator = sc.next().charAt(0);
+            System.out.print("사칙연산 기호 입력: ");
+            char operator = sc.next().charAt(0);
 
+            if (a < 0 || b < 0) {
+                System.out.println("0을 포함한 양의 정수만 입력할 수 있습니다.");
+                return;
+            }
 
-        if (a < 0 || b < 0) {
-            System.out.println("0을 포함한 양의 정수만 입력할 수 있습니다.");
-            return;
-        }
+            int result = 0;     // 연산 결과
+            boolean isResultValid = false;  // 연산 도중 문제 여부 flag
 
-        int result = 0;     // 연산 결과
-        boolean isResultValid = false;  // 연산 도중 문제 여부 flag
-
-        // TODO: 연산의 결과가 자료형의 범위를 벗어났을 때의 처리 방법을 생각해 봐야함
-        /*
-            - long 타입으로 먼저 계산한 뒤 오버플로우/언더플로우 체크 => 결과가 long 범위보다 크면??
-            - 래퍼클래스에는 이러한 예외를 처리할 수 있는 방법이 있지 않을까?
-         */
-        switch (operator) {
-            case '+':
-                result = a + b;
-                isResultValid = true;
-                break;
-            case '-':
-                result = a - b;
-                isResultValid = true;
-                break;
-            case '*':
-                result = a * b;
-                isResultValid = true;
-                break;
-            case '/':
-                if (b == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            // TODO: 연산의 결과가 자료형의 범위를 벗어났을 때의 처리 방법을 생각해 봐야함
+            /*
+                - long 타입으로 먼저 계산한 뒤 오버플로우/언더플로우 체크 => 결과가 long 범위보다 크면??
+                - 래퍼클래스에는 이러한 예외를 처리할 수 있는 방법이 있지 않을까?
+             */
+            switch (operator) {
+                case '+':
+                    result = a + b;
+                    isResultValid = true;
                     break;
-                }
-                result = a / b;
-                isResultValid = true;
+                case '-':
+                    result = a - b;
+                    isResultValid = true;
+                    break;
+                case '*':
+                    result = a * b;
+                    isResultValid = true;
+                    break;
+                case '/':
+                    if (b == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        break;
+                    }
+                    result = a / b;
+                    isResultValid = true;
+                    break;
+                default:
+                    System.out.println("올바르지 않은 연산자 입력입니다.");
+            }
+
+            if (isResultValid) {
+                System.out.println("계산 결과: " + result);
+            }
+
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+            String exitStr = sc.next();
+            if (exitStr.equals("exit")) {
                 break;
-            default:
-                System.out.println("올바르지 않은 연산자 입력입니다.");
+            }
         }
-
-        if (isResultValid) {
-            System.out.println("계산 결과: " + result);
-        }
-
     }
 
 }
