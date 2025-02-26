218d0bc0224ad26c119f1b01b0556b20611b1b66
SeungWoo Ryu <swstar21c@gmail.com>
2025-02-26 21:28:41 +0900

    #12 - OperatorType에 함수형 인터페이스 적용
    - 연산자 정보를 가지고 있는 Enum 타입인 OperatorType에 함수형 인터페이스를 사용해 연산 기능을 추가했다.
    - 기존의 switch 구문, flag 변수 등을 더 이상 사용하지 않게 되어 코드를 압축하고 가독성을 높일 수 있었다.

--- src/xyz/ncookie/calculator/ArithmeticCalculator.java
+++ src/xyz/ncookie/calculator/ArithmeticCalculator.java
@@ -3,7 +3,6 @@ package xyz.ncookie.calculator;
 import xyz.ncookie.calculator.data.OperatorType;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
@@ -12,46 +11,29 @@ public class ArithmeticCalculator {
 
     protected Queue<BigDecimal> resultQueue = new LinkedList<>();
 
-    public <T extends Number> Optional<BigDecimal> calculate(T a, T b, OperatorType operatorType) {
-        BigDecimal result = BigDecimal.ZERO;     // 연산 결과
-        boolean isResultValid = false;           // 연산 도중 문제 여부 flag
+    public <T> Optional<BigDecimal> calculate(T a, T b, OperatorType operatorType) {
+        BigDecimal result;     // 연산 결과
 
+        if (operatorType == null) {
+            System.out.println("올바르지 않은 연산자 입력입니다.");
+            return Optional.empty();
+        }
+
+        // 어떤 데이터 타입이 들어오더라도 BigDecimal 타입으로 변환
         BigDecimal bdA = new BigDecimal(String.valueOf(a));
         BigDecimal bdB = new BigDecimal(String.valueOf(b));
 
-        switch (operatorType) {
-            case ADD:
-                result = bdA.add(bdB);
-                isResultValid = true;
-                break;
-            case SUB:
-                result = bdA.subtract(bdB);
-                isResultValid = true;
-                break;
-            case MUL:
-                result = bdA.multiply(bdB);
-                isResultValid = true;
-                break;
-            case DIV:
-                if (bdB.equals(BigDecimal.ZERO)) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    break;
-                }
-                result = bdA.divide(bdB, RoundingMode.HALF_EVEN);
-                isResultValid = true;
-                break;
-            default:
-                System.out.println("올바르지 않은 연산자 입력입니다.");
-        }
-
-        // 정상적으로 연산이 수행되었을 때에만 결과 출력
-        if (isResultValid) {
+        try {
+            // 연산 수행 후 결과 저장 및 반환
+            result = operatorType.calculate(bdA, bdB);
             resultQueue.offer(result);
             return Optional.of(result);
-        } else {
-            // 연산 도중 문제가 발생했다면 null 반환
+        } catch (ArithmeticException e) {
+            // 연산 도중 에러가 발생했다면 null 반환
+            System.out.println(e.getMessage());
             return Optional.empty();
         }
+
     }
 
     public Queue<BigDecimal> getResultQueue() {
