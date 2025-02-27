2805862c1e3353fe278dd383683937d9ea602e28
SeungWoo Ryu <swstar21c@gmail.com>
2025-02-27 11:19:34 +0900

    #15 - main 메소드의 출력 기능 분리
    - 연산 결과, 에러 등을 출력하는 부분을 OutputHandler에서 다루도록 분리
    - 다양한 print 기능을 구현함

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,20 @@
 import calculator.ArithmeticCalculator;
 import data.CalcParam;
-import data.OperatorType;
 import exception.InvalidOperatorException;
 import io.InputHandler;
+import io.OutputHandler;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    static final String SEPARATOR = "======================================\n";
-
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+
         ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator();
 
         // "exit" 키워드 입력 받기 전까지는 무한루프
@@ -25,10 +25,10 @@ public class Main {
             try {
                 calcParam = inputHandler.readParams();
             } catch (NumberFormatException e) {
-                System.out.println("유효하지 않은 형식의 숫자입니다.");
+                outputHandler.printError("유효하지 않은 형식의 숫자입니다.");
                 continue;
             } catch (InvalidOperatorException e) {
-                System.out.println(e.getMessage());
+                outputHandler.printError(e.getMessage());
                 continue;
             }
 
@@ -38,19 +38,26 @@ public class Main {
                     calcParam.numX(),
                     calcParam.numY(),
                     calcParam.operatorType());
-            optional.ifPresent(result -> System.out.println("계산 결과: " + result));
+            optional.ifPresent(result -> outputHandler.printCalcResult(String.valueOf(result)));
 
-            System.out.println("연산 결과 데이터: " + arithmeticCalculator.getResultQueue());
-            List<BigDecimal> resultsBiggerThanInput = arithmeticCalculator.getResultQueue().stream()
-                    .filter(v -> v.compareTo(calcParam.numX()) > 0 && v.compareTo(calcParam.numY()) > 0)
-                    .toList();
-            System.out.println("입력값보다 큰 연산 결과 데이터: " + resultsBiggerThanInput);
-            System.out.println(SEPARATOR);
+            outputHandler.printCustomMsg(
+                    "연산 결과 기록",
+                    arithmeticCalculator.getResultQueue().stream()
+                            .map(String::valueOf).collect(Collectors.joining(", "))
+            );
+            outputHandler.printCustomMsg(
+                    "입력값보다 큰 연산 결과",
+                    arithmeticCalculator.getResultQueue().stream()
+                            .filter(v -> v.compareTo(calcParam.numX()) > 0 && v.compareTo(calcParam.numY()) > 0)
+                            .map(String::valueOf).collect(Collectors.joining(", "))
+            );
 
             // "exit"를 입력받으면 루프 종료
             if (inputHandler.readExitFlag()) {
                 break;
             }
+
+            outputHandler.printSeparator();
 
         }
     }
---
+++ src/main/io/OutputHandler.java
@@ -0,0 +1,28 @@
+package io;
+
+import java.math.BigDecimal;
+import java.util.Queue;
+
+public class OutputHandler {
+
+    static final String SEPARATOR = "======================================\n";
+
+    public void printCalcResult(String msg) {
+        System.out.println("***********************");
+        System.out.println("연산 결과: " + msg);
+        System.out.println("***********************");
+    }
+
+    public void printCustomMsg(String tag, String msg) {
+        System.out.printf("[%s] %s\n", tag, msg);
+    }
+
+    public void printError(String msg) {
+        System.out.println("[ERROR] " + msg);
+    }
+
+    public void printSeparator() {
+        System.out.println(SEPARATOR);
+    }
+
+}
